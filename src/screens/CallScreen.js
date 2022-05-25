2feb0c83611fac6f4c7cef60b0f852a302119ea1
Princess May Garde <princessmaygarde@gmail.com>
2022-05-25 23:53:16 +0800

    mute button ui

--- src/screens/CallScreen.js
+++ src/screens/CallScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Button, Text, TextInput,Dimensions, TouchableOpacity } from 'react-native';
 import ConnectyCube from 'react-native-connectycube';
 import { Avatar } from 'react-native-paper';
@@ -8,15 +8,32 @@ import { RTCView } from 'react-native-connectycube';
 const CallScreen = (response) => {
 
   const RTCViews = () => {
+    const [isMuted, setMute] = useState(false);
     const res = response.route.params;
+
+    const _muteCall = () => {
+      if (isMuted == false) {
+        setMute(true)
+      } else {
+        setMute(false)
+      }
+    }
+
     if(res.localKey) {
       return (
         <View style={styles.blackView}>
           <RTCView style={styles.localKey} key={res.localKey} streamURL={res.localStream.toURL()} mirror={true}/>
           <RTCView style={styles.remoteKey} objectFit="cover" key={res.remoteKey} streamURL={res.remoteStream.toURL()} mirror={true}/>
-          <TouchableOpacity style={styles.dropCallButton}>
-            <Avatar.Icon size={50} icon="phone-hangup" style={{backgroundColor:"#ff4a43"}}/>
-          </TouchableOpacity>
+          <View style={styles.dropCallButton}>
+            <View style={{display: 'flex', flexDirection: 'row'}}>
+              <TouchableOpacity style={{marginHorizontal: 5}} onPress={_muteCall}>
+                <Avatar.Icon size={50} icon={isMuted ? "microphone-off" : "microphone"} style={{backgroundColor:"white"}}/>
+              </TouchableOpacity>
+              <TouchableOpacity style={{marginHorizontal: 5}}>
+                <Avatar.Icon size={50} icon="phone-hangup" style={{backgroundColor:"#ff4a43"}}/>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       );
     }
