e4edb03da315404b58475c4fa30634899c5c9172
Princess May Garde <princessmaygarde@gmail.com>
2022-11-08 00:20:57 +0800

    update

--- src/screens/OrdersScreen.js
+++ src/screens/OrdersScreen.js
@@ -86,10 +86,6 @@ const Orders = ({ navigation }: Props) => {
 
   const formatNumber = (inputNumber) => {
     let formetedNumber=(Number(inputNumber)).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-    let splitArray=formetedNumber.split('.');
-    if(splitArray.length>1){
-      formetedNumber=splitArray[0];
-    }
     return(formetedNumber);
   }
 
