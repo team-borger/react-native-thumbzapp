5c57f1654e6dfac8df2b67fba07082f14ca3ffbd
Princess May Garde <princessmaygarde@gmail.com>
2022-11-13 22:06:30 +0800

    updates

--- src/screens/PaymentSuccessScreen.js
+++ src/screens/PaymentSuccessScreen.js
@@ -15,6 +15,7 @@ type Props = {
 
 const Checkout = ({ navigation }: Props) => {
   const [subTotal, setTotal] = useState(0)
+  const [payMethod, setPayment] = useState({})
 
   useEffect(() => {
     _getCheckout()
@@ -23,9 +24,14 @@ const Checkout = ({ navigation }: Props) => {
   const _getCheckout = async () => {
     try {
       const value = await AsyncStorage.getItem('checkout')
+      const payment = await AsyncStorage.getItem('paymentMethod')
       if (value !== null) {
         const ret = JSON.parse(value);
         setsubtotal(ret)
+      }
+      if (payment !== null) {
+        const pay = JSON.parse(payment);
+        setPayment(pay)
       }
     } catch (error) {
       console.log('error async storage')
@@ -60,7 +66,8 @@ const Checkout = ({ navigation }: Props) => {
 
       <View style={styles.contentContainer}>
         <View style={{flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', marginHorizontal: 20}}>
-          <Text style={{color: '#880ED4', fontSize: 24, fontWeight: 'bold'}}><FontAwesome name='check-circle' size={24} color='#880ED4' /> You paid {'\u20B1'} {subTotal}</Text>
+          <Text style={ payMethod.method_type === 'Cash on Delivery' ? {display: 'none'} : {color: '#880ED4', fontSize: 24, fontWeight: 'bold'}}><FontAwesome name='check-circle' size={24} color='#880ED4' /> You paid {'\u20B1'} {subTotal}</Text>
+          <Text style={ payMethod.method_type === 'Cash on Delivery' ? {color: '#880ED4', fontSize: 24, fontWeight: 'bold', textAlign: 'center'} : {display: 'none'}}><FontAwesome name='check-circle' size={24} color='#880ED4' /> You have a total of {'\u20B1'} {subTotal} pending payment.</Text>
           <Text style={{color: '#880ED4', fontSize: 16, textAlign: 'center', marginTop: 10}}>We have notified the seller to ship out your order.</Text>
           <View style={{marginTop: 40}}>
             <Button style={styles.logoutBtn} mode="outlined" color="#880ED4" onPress={_goBack}>
