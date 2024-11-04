f57c653015d729a99ba8588818908bf2ecd302fb
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2024-11-04 18:51:52 +0800

    scrolling action

--- src/screens/AuthRouteLanding.js
+++ src/screens/AuthRouteLanding.js
@@ -1,18 +1,23 @@
-import DashboardCard from '../components/DashboardCard';
+import React from 'react';
+import { ScrollView } from 'react-native';
 import { Card } from 'react-native-paper';
+import DashboardCard from '../components/DashboardCard';
 
 export default function Main({ navigation }) {
   const handleCardPress = (route) => {
     console.log('DashboardCard was clicked!');
-    navigation.navigate(route)
+    navigation.navigate(route);
   };
+
   return (
-    <Card>
-      <DashboardCard onPress={() => handleCardPress('Marketplace')}/>
-      <DashboardCard onPress={() => handleCardPress('Load')}/>
-      <DashboardCard onPress={() => handleCardPress('Ticketing')}/>
-      <DashboardCard onPress={() => handleCardPress('Profile')}/>
-      <DashboardCard onPress={() => handleCardPress('Dashboard')}/>
-    </Card>
+    <ScrollView contentContainerStyle={{ paddingVertical: 10 }}>
+      <Card>
+        <DashboardCard onPress={() => handleCardPress('Marketplace')} />
+        <DashboardCard onPress={() => handleCardPress('Load')} />
+        <DashboardCard onPress={() => handleCardPress('Ticketing')} />
+        <DashboardCard onPress={() => handleCardPress('Profile')} />
+        <DashboardCard onPress={() => handleCardPress('Dashboard')} />
+      </Card>
+    </ScrollView>
   );
 }
